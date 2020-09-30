e552582150bc302bb36a67649e21d9f37ae7293b
Guy Greenleaf <yaweh23@gmail.com>
2020-09-29 22:43:28 -0700

    Added btn shadows and keyboardpress.html

--- index.js
+++ index.js
@@ -10,8 +10,10 @@ let whatsThis = () => location.href = "whatsThis.html";
 
 let homeMenu = () => location.href = "index.html";
 
+let keyboardPress = () => location.href = "keyboardPress.html"
+
 let contactLoad = () => location.href = "contact.html";
+
 $(function() {
     $('body').removeClass('fade-out');
 });
-
